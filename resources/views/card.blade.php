9ff8a84ce8dc1e3e1fbd51dc2a4ba3e786363f17
Dan Anderson <anderson@familymedia.org>
2018-08-17 16:19:17 -0400

    Try Something

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -4,13 +4,14 @@
     <section class="jumbotron text-center">
         <div class="container">
             <div class="frame my-5">
-                <img src="{{ $card['card'] }}" class="img-responsive" />
+                <img src="{{ asset('/gilded-frame.png') }}" class="img-responsive gilded" />
+                <img src="{{ $card['card'] }}" class="img-responsive artwork" />
             </div>
             <h1 class="jumbotron-heading">{{ $card['name'] }}</h1>
             <p class="lead text-muted">{{ $card['burned'] }} of {{ $card['issued'] }}</p>
             <p>
-                <a href="{{ $card['link'] }}" class="btn btn-primary my-2" target="_blank">More</a>
-                <a href="{{ route('home') }}" class="btn btn-secondary my-2">&raquo; Back</a>
+                <a href="{{ route('home') }}" class="btn btn-secondary my-2">&laquo; Back</a>
+                <a href="{{ $card['link'] }}" class="btn btn-primary my-2" target="_blank">More &raquo;</a>
             </p>
         </div>
     </section>
