d419d3538cfba04ef2f2e844d1d272c470a416f2
Dan Anderson <anderson@familymedia.org>
2018-08-17 17:00:15 -0400

    Fix JS Maybe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,8 +42,6 @@
         // Existing code unchanged.
         window.onload = function() {
           var context = new AudioContext();
-          // Setup all nodes
-          ...
         }
 
         // One-liner to resume playback when user interacted with the page.
