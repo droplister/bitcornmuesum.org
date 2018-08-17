9ba5c57b7825c89e6be728eb7b77daaf6c852a94
Dan Anderson <anderson@familymedia.org>
2018-08-17 16:04:20 -0400

    Fix Reference

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -9,7 +9,7 @@
             <h1 class="jumbotron-heading">{{ $card['name'] }}</h1>
             <p class="lead text-muted">{{ $card['burned'] }} of {{ $card['issued'] }}</p>
             <p>
-                <a href="{{ $card['url'] }}" class="btn btn-primary my-2" target="_blank">More</a>
+                <a href="{{ $card['link'] }}" class="btn btn-primary my-2" target="_blank">More</a>
                 <a href="{{ route('home') }}" class="btn btn-secondary my-2">&raquo; Back</a>
             </p>
         </div>
